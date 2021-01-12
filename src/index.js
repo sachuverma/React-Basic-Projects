40fdf0fc2d05c5c69c727819177b4b46b4597748
Sachin Verma <sachinverma53121@gmail.com>
2021-01-12 12:19:16 +0530

    props

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import ReactDom from "react-dom";
 // CSS
 import "./index.css";
 
+// setup vars
+const firstBook = {
+  img:
+    "https://images-na.ssl-images-amazon.com/images/I/51KkyqK7avL._AC_SX184_.jpg",
+  title: "Wintering: The Power of Rest and Retreat in Difficult Times",
+  author: "Katherine May",
+};
+
 // JSX RULES
 /**
  * return single element
@@ -17,23 +25,26 @@ import "./index.css";
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
+      <Book
+        title={firstBook.title}
+        author={firstBook.author}
+        img={firstBook.img}
+      />
+      <Book
+        title="Think Like a Monk: The secret of how to harness the power of positivity and be happy now"
+        author="Jay Shetty"
+        img="https://images-na.ssl-images-amazon.com/images/I/41gVhoPaE5L._SX323_BO1,204,203,200_.jpg"
+      />
     </section>
   );
 }
 
-const Book = () => {
-  const title = "Wintering: The Power of Rest and Retreat in Difficult Times",
-    author = "Katherine May";
-
+const Book = (props) => {
   return (
     <article className="book">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/51KkyqK7avL._AC_SX184_.jpg"
-        alt="book cover page"
-      />
-      <h1> {title}</h1>
-      <p> {author.toUpperCase()} </p>
+      <img src={props.img} alt="book cover page" />
+      <h1> {props.title}</h1>
+      <p> {props.author.toUpperCase()}</p>
     </article>
   );
 };
