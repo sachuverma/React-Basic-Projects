80a89c0a166f0b94ae91513c9821c7ae2a3c00b6
Sachin Verma <sachinverma53121@gmail.com>
2021-01-12 18:32:21 +0530

    use effect basics

--- starter/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ starter/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -5,8 +5,8 @@ import React, { useState, useEffect } from "react";
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
   useEffect(() => {
-    document.title = `New Messages ${value}`;
-  });
+    if (value > 0) document.title = `New Messages (${value})`;
+  }, [value]);
 
   return (
     <div>
