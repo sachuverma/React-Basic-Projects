112132fecb5a47bdd16269188ca6d381fba28588
Sachin Verma <sachinverma53121@gmail.com>
2021-01-12 11:46:05 +0530

    jsx JS

--- src/index.js
+++ src/index.js
@@ -23,14 +23,17 @@ function BookList() {
 }
 
 const Book = () => {
+  const title = "Wintering: The Power of Rest and Retreat in Difficult Times",
+    author = "Katherine May";
+
   return (
     <article className="book">
       <img
         src="https://images-na.ssl-images-amazon.com/images/I/51KkyqK7avL._AC_SX184_.jpg"
         alt="book cover page"
       />
-      <h1> Wintering: The Power of Rest and Retreat in Difficult Times</h1>
-      <p> Katherine May </p>
+      <h1> {title}</h1>
+      <p> {author.toUpperCase()} </p>
     </article>
   );
 };
