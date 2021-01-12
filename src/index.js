800e7dcb1d090dc1fa575b210dc9dce3b27baa91
Sachin Verma <sachinverma53121@gmail.com>
2021-01-12 12:26:20 +0530

    prop children

--- src/index.js
+++ src/index.js
@@ -29,24 +29,35 @@ function BookList() {
         title={firstBook.title}
         author={firstBook.author}
         img={firstBook.img}
-      />
+      >
+        <p>
+          vitae soluta nihil error magnam inventore pariatur eos perspiciatis
+          quasi id nemo ea?
+        </p>
+      </Book>
       <Book
         title="Think Like a Monk: The secret of how to harness the power of positivity and be happy now"
         author="Jay Shetty"
         img="https://images-na.ssl-images-amazon.com/images/I/41gVhoPaE5L._SX323_BO1,204,203,200_.jpg"
-      />
+      >
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+          voluptatum molestiae, dolores ratione omnis voluptatem in, quibusdam
+        </p>
+      </Book>
     </section>
   );
 }
 
-const Book = ({ img, title, author }) => {
+const Book = ({ img, title, author, children }) => {
   // const { img, title, author } = props;
 
   return (
     <article className="book">
       <img src={img} alt="book cover page" />
       <h1> {title}</h1>
-      <p> {author.toUpperCase()}</p>
+      <h4> {author.toUpperCase()}</h4>
+      {children}
     </article>
   );
 };
