2735028d79d35f2eea5f92b3cfbc552fb50a37a3
Sachin Verma <sachinverma53121@gmail.com>
2021-01-12 12:52:59 +0530

    event handlers

--- src/index.js
+++ src/index.js
@@ -69,12 +69,33 @@ function BookList() {
 const Book = ({ img, title, author, children }) => {
   // const { img, title, author } = props;
 
+  // attribute, eventHandler
+  // onClick, onMouseHover
+  const complexExample = (author) => {
+    console.log("author name is: ", author);
+  };
+
   return (
-    <article className="book">
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
       <img src={img} alt="book cover page" />
-      <h1> {title}</h1>
-      <h4> {author.toUpperCase()}</h4>
+      <h1
+        onClick={(e) => {
+          console.log(e);
+          return alert(`${title}: ${author}`);
+        }}
+      >
+        {title}
+      </h1>
+      <h4>{author.toUpperCase()}</h4>
       {children}
+      <button type="button" onClick={() => complexExample(author)}>
+        complex example
+      </button>
     </article>
   );
 };
