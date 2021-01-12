957aaf10141839042acf224d7a1d44992f1d0ef2
Sachin Verma <sachinverma53121@gmail.com>
2021-01-12 16:01:46 +0530

    use state object

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import UseStateBasics from "./tutorial/1-useState/setup/2-useState-basics";
 import UseStateArray from "./tutorial/1-useState/setup/3-useState-array";
+import UseStateObject from "./tutorial/1-useState/setup/4-useState-object";
 
 function App() {
   return (
     <div className="container">
       <UseStateBasics />
       <UseStateArray />
+      <UseStateObject />
     </div>
   );
 }
--- starter/src/tutorial/1-useState/setup/4-useState-object.js
+++ starter/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "sachu",
+    age: 20,
+    message: "this is random message",
+  });
+
+  const changeMessage = () => {
+    setPerson({ ...person, message: "hello world!" });
+  };
+
+  return (
+    <div>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        change message
+      </button>
+    </div>
+  );
 };
 
 export default UseStateObject;
