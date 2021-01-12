9a91b23bc1b037201efc86172f4009819fe7e2d0
Sachin Verma <sachinverma53121@gmail.com>
2021-01-12 18:19:35 +0530

    use effect basics

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+
 import UseStateBasics from "./tutorial/1-useState/setup/2-useState-basics";
 import UseStateArray from "./tutorial/1-useState/setup/3-useState-array";
 import UseStateObject from "./tutorial/1-useState/setup/4-useState-object";
 import UseStateCounter from "./tutorial/1-useState/setup/5-useState-counter";
+
+import UseEffectBasics from "./tutorial/2-useEffect/setup/1-useEffect-basics";
 
 function App() {
   return (
@@ -11,6 +14,8 @@ function App() {
       <UseStateArray />
       <UseStateObject />
       <UseStateCounter />
+
+      <UseEffectBasics />
     </div>
   );
 }
--- starter/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ starter/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    document.title = `New Messages ${value}`;
+  });
+
+  return (
+    <div>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        inc counter
+      </button>
+    </div>
+  );
 };
 
 export default UseEffectBasics;
