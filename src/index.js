fe316d671c006418da600f39f67d211da0201259
Sachin Verma <sachinverma53121@gmail.com>
2021-01-12 12:42:37 +0530

    prop spread operator

--- src/index.js
+++ src/index.js
@@ -5,12 +5,45 @@ import ReactDom from "react-dom";
 import "./index.css";
 
 // setup vars
-const firstBook = {
-  img:
-    "https://images-na.ssl-images-amazon.com/images/I/51KkyqK7avL._AC_SX184_.jpg",
-  title: "Wintering: The Power of Rest and Retreat in Difficult Times",
-  author: "Katherine May",
-};
+const books = [
+  {
+    id: 1,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/51KkyqK7avL._AC_SX184_.jpg",
+    title: "Wintering: The Power of Rest and Retreat in Difficult Times",
+    author: "Katherine May",
+  },
+  {
+    id: 2,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/41gVhoPaE5L._SX323_BO1,204,203,200_.jpg",
+    title:
+      "Think Like a Monk: The secret of how to harness the power of positivity and be happy now",
+    author: "Jay Shetty",
+  },
+  {
+    id: 3,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/41ThX3SfAiL._SX329_BO1,204,203,200_.jpg",
+    title: "The Vanishing Half",
+    author: "Brit Bennett",
+  },
+  {
+    id: 4,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/41U3yoF6sXL._SX427_BO1,204,203,200_.jpg",
+    title: "Greenlights ",
+    author: "Matthew McConaughey",
+  },
+  {
+    id: 5,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/51Tlm0GZTXL._SX329_BO1,204,203,200_.jpg",
+    title:
+      "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+    author: "James Clear",
+  },
+];
 
 // JSX RULES
 /**
@@ -25,21 +58,10 @@ const firstBook = {
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        title={firstBook.title}
-        author={firstBook.author}
-        img={firstBook.img}
-      >
-        <p>
-          vitae soluta nihil error magnam inventore pariatur eos perspiciatis
-          quasi id nemo ea?
-        </p>
-      </Book>
-      <Book
-        title="Think Like a Monk: The secret of how to harness the power of positivity and be happy now"
-        author="Jay Shetty"
-        img="https://images-na.ssl-images-amazon.com/images/I/41gVhoPaE5L._SX323_BO1,204,203,200_.jpg"
-      ></Book>
+      {books.map((book) => {
+        const { id, img, title, author } = book;
+        return <Book key={book.id} {...book} />;
+      })}
     </section>
   );
 }
