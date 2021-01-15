c0a3c29488d2622ee0b2dbb83d1168ccc046d7e1
Sachin Verma <sachinverma53121@gmail.com>
2021-01-15 15:26:11 +0530

    useref

--- `practice`/src/App.js
+++ `practice`/src/App.js
@@ -16,6 +16,8 @@ import ShowHide from "./tutorial/3-conditional-rendering/setup/3-show-hide";
 import ControlledInput from "./tutorial/4-forms/setup/1-controlled-inputs";
 import MultipleInput from "./tutorial/4-forms/setup/2-multiple-inputs";
 
+import UseRefBasics from "./tutorial/5-useRef/setup/1-useRef-basics";
+
 function App() {
   return (
     <div className="container">
@@ -36,6 +38,8 @@ function App() {
 
       <ControlledInput />
       <MultipleInput />
+
+      <UseRefBasics />
     </div>
   );
 }
