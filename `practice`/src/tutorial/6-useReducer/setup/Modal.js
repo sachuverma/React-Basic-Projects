b7cc2e2558f0fe2515d8142671ea74124e1af72c
Sachin Verma <sachinverma53121@gmail.com>
2021-01-16 22:56:01 +0530

    reducer remove items

--- `practice`/src/tutorial/6-useReducer/setup/Modal.js
+++ `practice`/src/tutorial/6-useReducer/setup/Modal.js
@@ -1,6 +1,13 @@
 import React, { useEffect } from "react";
 
-const Modal = ({ modalContent }) => {
+const Modal = ({ modalContent, closeModal }) => {
+  useEffect(() => {
+    let timeout = setTimeout(() => {
+      closeModal();
+    }, 3000);
+    return () => clearTimeout(timeout);
+  });
+
   return (
     <div className="modal">
       <p>{modalContent}</p>
--- `practice`/src/tutorial/6-useReducer/setup/index.js
+++ `practice`/src/tutorial/6-useReducer/setup/index.js
@@ -20,6 +20,22 @@ const reducer = (state, action) => {
       modalContent: "please enter value",
     };
   }
+  if (action.type === "CLOSE_MODAL") {
+    return {
+      ...state,
+      isModalOpen: false,
+    };
+  }
+  if (action.type === "REMOVE_ITEM") {
+    const id = action.payload;
+    const newPeople = state.people.filter((person) => person.id !== id);
+    return {
+      ...state,
+      people: newPeople,
+      isModalOpen: true,
+      modalContent: "item removed",
+    };
+  }
   // return state;
   throw new Error("no matching action type");
 };
@@ -46,9 +62,15 @@ const Index = () => {
     }
   };
 
+  const closeModal = () => {
+    dispatch({ type: "CLOSE_MODAL" });
+  };
+
   return (
     <div>
-      {state.isModalOpen && <Modal modalContent={state.modalContent} />}
+      {state.isModalOpen && (
+        <Modal modalContent={state.modalContent} closeModal={closeModal} />
+      )}
       <form onSubmit={handleSubmit} className="form">
         <div>
           <input
@@ -61,8 +83,15 @@ const Index = () => {
       </form>
       {state.people.map((person) => {
         return (
-          <div key={person.id}>
+          <div key={person.id} className="item">
             <h4>{person.name}</h4>
+            <button
+              onClick={() =>
+                dispatch({ type: "REMOVE_ITEM", payload: person.id })
+              }
+            >
+              remove
+            </button>
           </div>
         );
       })}
