b09945e3281efb7fdb7dd7c99873c848c3c4e0cc
Sachin Verma <sachinverma53121@gmail.com>
2021-01-17 19:12:21 +0530

    cocktail fetching

--- cocktails/src/components/SearchForm.js
+++ cocktails/src/components/SearchForm.js
@@ -1,12 +1,34 @@
-import React from 'react'
-import { useGlobalContext } from '../context'
+import React from "react";
+
+import { useGlobalContext } from "../context";
 
 const SearchForm = () => {
-  return (
-    <div>
-      <h2>search form component</h2>
-    </div>
-  )
-}
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = React.useRef("");
 
-export default SearchForm
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  return (
+    <section className="section search">
+      <form className="search-form" onSubmit={(e) => e.preventDefault()}>
+        <div className="form-control">
+          <label htmlFor="name">search your favourite cocktail...</label>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default SearchForm;
