9eaab3df7d8fb98a4b2dc9139883839c6902fbbb
Sachin Verma <sachinverma53121@gmail.com>
2021-01-17 12:02:01 +0530

    stripe hero

--- stripe menu/src/Hero.js
+++ stripe menu/src/Hero.js
@@ -5,8 +5,26 @@ import phoneImg from "./images/phone.svg";
 import { useGlobalContext } from "./context";
 
 const Hero = () => {
-  const myData = useGlobalContext();
-  return <h2>hero component</h2>;
+  const { closeSubmenu } = useGlobalContext();
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>lorem ipsum </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi velit
+            animi obcaecati, nam ipsa sed maiores consectetur tempora optio
+            doloremque cupiditate tenetur asperiores soluta esse debitis odit ea
+            corrupti. Cumque! lorem
+          </p>
+          <button className="btn">Start Now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} alt="phone image" className="phone-img" />
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
