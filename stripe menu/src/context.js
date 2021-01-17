cb1c62652d8c7e40ebda0c9f9841fb926beeae47
Sachin Verma <sachinverma53121@gmail.com>
2021-01-17 12:08:02 +0530

    stripe side bar

--- stripe menu/src/context.js
+++ stripe menu/src/context.js
@@ -5,7 +5,7 @@ import sublinks from "./data";
 const AppContext = React.createContext();
 
 export const AppProvider = ({ children }) => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
 
   const openSidebar = () => {
